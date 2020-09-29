8ca17173d0e6cb1e0b0d57deab4632111eb82c43
helvetica7777 <54688265+helvetica7777@users.noreply.github.com>
2020-09-29 16:20:55 -0400

    1

--- Scripts/GhostGenerator.cs
+++ Scripts/GhostGenerator.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class GhostGenerator : MonoBehaviour
 {
+    public GameObject ghost;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(ghost, new Vector3(0, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
